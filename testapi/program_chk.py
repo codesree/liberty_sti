1b0db9b985ab391e3d9b3bfc2012e4090922ed5b
codesree <srekanth.me@gmail.com>
2018-10-14 15:37:04 +0200

    updated settings & ngnix for staticpath

--- testapi/program_chk.py
+++ testapi/program_chk.py
@@ -1,9 +1,15 @@
 
 from pymongo import MongoClient
 import requests
-
+import os
 # db = MongoClient('mongodb://ds117423.mlab.com:17423',
 #                       username='srekanth',
 #                       password='liberty@123',
 #                       authSource='liberty_sti',
 #                       authMechanism='SCRAM-SHA-1')
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_ROOT=os.path.join(BASE_DIR, 'static/')
+
+print(STATIC_ROOT)
